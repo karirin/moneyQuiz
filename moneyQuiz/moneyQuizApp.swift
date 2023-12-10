f937d3305ac5330e29830e1318d3d0ec590539fe
karirin <karirin419@gmail.com>
2023-12-10 18:15:44 +0900

    Initial Commit

---
+++ moneyQuiz/moneyQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  moneyQuizApp.swift
+//  moneyQuiz
+//
+//  Created by hashimo ryoya on 2023/12/10.
+//
+
+import SwiftUI
+
+@main
+struct moneyQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
